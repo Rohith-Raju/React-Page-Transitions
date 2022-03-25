9e0817d4b11f6157512a7c8e23b3baaa9f467c10
rohith-cmd <rohithraju488@gmail.com>
2022-03-25 21:13:31 +0530

    need to make it smoother and work properly

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,27 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSpring, animated, easings } from 'react-spring';
 
 //styles import
 import styles from '../styles/Slider.module.css';
 
 const Slider: React.FC = () => {
-  const [click, setClick] = useState(false);
+  const [load, setLoad] = useState(false);
 
-  const props: object = useSpring({
+  const loader: object = useSpring({
     config: { duration: 1500, easing: easings.easeInOutQuint },
-    to: [{ width: click ? '100%' : '0%' }, { left: click ? '100%' : '0' }],
+    to: [{ width: load ? '100%' : '0%' }, { left: load ? '100%' : '0' }],
+    reset: true,
   });
 
+  const text: object = useSpring({
+    config: { duration: 1500, easing: easings.easeInOutElastic },
+    from: { transform: 'translate(0px,100px)', opacity: 0 },
+    to: [
+      { transform: 'translate(0px,0px)', opacity: 1 },
+      { transfrom: 'translate(0px,-100px)', opacity: 0 },
+    ],
+    reset: true,
+  });
+
+  useEffect(() => {
+    setLoad(true);
+  }, []);
+
   return (
-    <>
-      <button onClick={() => setClick(true)}>click</button>
+    <div className={styles.container}>
       <div className={styles.loadcontainer}>
         <animated.div
-          style={props}
+          style={loader}
           className={styles.loadscreen1}
         ></animated.div>
       </div>
-    </>
+      <animated.div style={text} className={styles.text}>
+        <h1>hello</h1>
+      </animated.div>
+    </div>
   );
 };
 
